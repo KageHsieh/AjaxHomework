6d1669c2c63ba9bcf7d89396d3e0a0763161515d
KageHsieh <p268956again@gmail.com>
2023-07-13 18:01:31 +0800

    0713

---
+++ Controllers/ApiController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Msit147Site.Controllers
+{
+    public class ApiController : Controller
+    {
+        public IActionResult Index()
+        {
+            //return Content("Hello World");
+            return Content("<h2>Hello World!!</h2>", "text/html");
+        }
+    }
+}
